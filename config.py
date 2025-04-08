dffbce9f50b967499d1d6a95f1821b30ffbe3e90
Jonathanh0023 <142325028+Jonathanh0023@users.noreply.github.com>
2025-04-08 15:53:38 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # config.py
 import streamlit as st
 
-def set_page_config():d
+def set_page_config():
     st.set_page_config(
         page_title="bonsAI Übersetzungsbüro",
         page_icon="https://sw01.rogsurvey.de/data/bonsai/Kara_23_19/logo_Bonsa_BONSAI_neu.png",
